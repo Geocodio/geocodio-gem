7922b26250c725be42a3db69fa753500ed049ce7
GoldenPavilion <rcorystine@gmail.com>
2022-12-07 18:35:59 -0500

    added vcr tags to API tests

--- spec/geocodio/gem_spec.rb
+++ spec/geocodio/gem_spec.rb
@@ -2,6 +2,7 @@
 
 require 'dotenv'
 require 'pathname'
+require 'spec_helper'
 Dotenv.load
 
 RSpec.describe Geocodio do
@@ -18,7 +19,7 @@ RSpec.describe Geocodio do
     expect(geocodio.api_key).to be(api_key)
   end
 
-  it "geocodes a single address" do
+  it "geocodes a single address", :vcr do
     address_sample = ["1109 N Highland St, Arlington, VA 22201"]
 
     expect(geocodio.geocode(address_sample)["input"]["formatted_address"]).to eq(address_sample.join(""))
@@ -26,7 +27,7 @@ RSpec.describe Geocodio do
     expect(geocodio.geocode(address_sample)["results"][0]["address_components"]["number"]).to eq("1109")
   end
 
-  it "appends fields to single address" do
+  it "appends fields to single address", :vcr do
     address_sample = ["1109 N Highland St, Arlington, VA 22201"]
     appended_fields = ["school", "cd"]
 
@@ -36,40 +37,40 @@ RSpec.describe Geocodio do
     expect(geocodio.geocode(address_sample, appended_fields)["results"][0]["fields"]["school_districts"]["unified"]["grade_high"]).to eq("12")
   end
 
-  it "#geocode can limit amount of responses" do
+  it "#geocode can limit amount of responses", :vcr do
     address_sample = ["1109 N Highland St, Arlington, VA 22201"]
     appended_fields = ["school", "cd"]
     expect(geocodio.geocode(address_sample, appended_fields, 1)["results"].length).to eq(1)
   end
 
-  it "#geocode can return simple format" do
+  it "#geocode can return simple format", :vcr do
     address_sample = ["1109 N Highland St, Arlington, VA 22201"]
     expect(geocodio.geocode(address_sample, [], nil, "simple")["address"]).to eq(address_sample.join(""))
   end
 
-  it "reverse geocodes coordinates" do
+  it "reverse geocodes coordinates", :vcr do
     coords_sample = ["38.9002898,-76.9990361"]
     expect(geocodio.reverse(coords_sample)["results"][0]["location"]).to eq({"lat"=>38.900432, "lng"=>-76.999031})
   end
 
-  it "appends fields to coordinates" do
+  it "appends fields to coordinates", :vcr do
     coords_sample = ["38.9002898,-76.9990361"]
     appended_fields = ["school", "cd"]
     expect(geocodio.reverse(coords_sample, appended_fields)["results"][0]["fields"]["school_districts"]["unified"]["name"]).to eq("District of Columbia Public Schools")
   end
 
-  it "#reverse can limit amount of responses" do
+  it "#reverse can limit amount of responses", :vcr do
     coords_sample = ["38.9002898,-76.9990361"]
     appended_fields = ["school", "cd"]
     expect(geocodio.reverse(coords_sample, appended_fields, 1)["results"].length).to eq(1)
   end
 
-  it "#reverse can return simple format" do
+  it "#reverse can return simple format", :vcr do
     coords_sample = ["38.9002898,-76.9990361"]
     expect(geocodio.reverse(coords_sample, [], nil, "simple")["address"]).to eq("508 H St NE, Washington, DC 20002")
   end
 
-  it "batch geocodes multiple addresses" do
+  it "batch geocodes multiple addresses", :vcr do
     batch_addresses = [
       "1109 N Highland St, Arlington, VA 22201",
       "12187 Darnestown Rd, Gaithersburg, MD 20878",
@@ -78,7 +79,7 @@ RSpec.describe Geocodio do
     expect(geocodio.geocode(batch_addresses)["results"].size).to equal(batch_addresses.size)
   end
 
-  it "reverse geocodes batch coordinates" do
+  it "reverse geocodes batch coordinates", :vcr do
     batch_coordinates = [
       "38.88674717512318, -77.09464642536076",
       "39.118308110111954, -77.2516753863881",
@@ -87,7 +88,7 @@ RSpec.describe Geocodio do
     expect(geocodio.reverse(batch_coordinates)["results"].size).to equal(batch_coordinates.size)
   end
 
-  it "creates list from file" do
+  it "creates list from file", :vcr do
     file = "sample_list_test.csv"
     path = File.read(file)
     filename = "sample_list_test.csv" 
@@ -96,25 +97,25 @@ RSpec.describe Geocodio do
     expect(geocodio.createList(path, filename, "forward", format)["file"]["filename"]).to eq(filename)
   end
 
-  it "gets list using ID" do
+  it "gets list using ID", :vcr do
     expect(geocodio.getList(11533825)["id"]).to be(11533825)
   end
 
-  it "gets all lists" do
+  it "gets all lists", :vcr do
     expect(geocodio.getAllLists.size).to be(9)
   end
 
-  it "downloads a list" do
+  it "downloads a list", :vcr do
     file = "sample_list_test.csv"
     path = File.read(file)
     filename = "sample_list_test.csv" 
     format = "{{A}} {{B}} {{C}} {{D}}"
     id = geocodio.createList(path, filename, "forward", format)["id"]
-    byebug
+
     expect(geocodio.downloadList(id)["success"]).to eq(false) 
   end
 
-  it "deletes a list" do
+  it "deletes a list", :vcr do
     file = "sample_list_test.csv"
     path = File.read(file)
     filename = "sample_list_test.csv" 
@@ -124,7 +125,3 @@ RSpec.describe Geocodio do
     expect(geocodio.deleteList(id)["success"]).to be(true)
   end
 end
-
-
-### WebMock Gem
-### VCR Gem
